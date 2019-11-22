fa08796cd1396e416cc1daf479b7a86f7f58a968
putu ary sukma bayu <putuarysukmabayu@gmail.com>
2019-11-22 18:42:21 +0700

    tambah sedikit ary

--- TubesUas_A_4/User/UserEventDeskripsiController.swift
+++ TubesUas_A_4/User/UserEventDeskripsiController.swift
@@ -7,24 +7,57 @@
 //
 
 import UIKit
+import UIKit
+import Alamofire
+import SwiftyJSON
 
+struct eventResponse: Decodable {
+    var id: String!
+    var nama: String!
+    var kategori: String!
+    var penyelenggara: String!
+    var tempat: String!
+    var waktu: String!
+    var deskripsi: String!
+    var no_rek: String!
+    var nama_rek: String!
+    var bank_rek: String!
+    var username: String!
+    var password: String!
+    var harga: String!
+}
 class UserEventDeskripsiController: UIViewController {
+    @IBOutlet weak var namaTxt: UILabel!
+    @IBOutlet weak var penyelenggaraTxt: UILabel!
 
+    @IBOutlet weak var deskripsiTxt: UILabel!
+    @IBOutlet weak var hargaTxt: UILabel!
+    @IBOutlet weak var lokasiTxt: UILabel!
+    let URL_Get_Event = "https://uajytix.xyz/REST-API/event/searchById.php?id="
+    var userId:String = ""
+    var eventId:String = ""
+    var eventData:eventResponse? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(self.eventId)
+        self.getData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getData(){
+        Alamofire.request(URL_Get_Event + eventId, method: .get).responseJSON{
+            response in
+            print(JSON(response.result.value!))
+            let statusCode = response.response?.statusCode
+            if(statusCode == 200){
+                do{
+                    let decoder = try JSONDecoder().decode(eventResponse.self, from: response.data!)
+                    self.eventData = decoder
+                    
+                }catch{
+                    print(Error.self)
+                }
+            }
+        }
     }
-    */
 
 }
--- TubesUas_A_4/User/UserMyTicketController.swift
+++ TubesUas_A_4/User/UserMyTicketController.swift
@@ -21,17 +21,6 @@ class UserMyTicketController: UIViewController {
         transaksi!.userId = self.userId
         profile!.userId = self.userId
         // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    }	
 
 }
